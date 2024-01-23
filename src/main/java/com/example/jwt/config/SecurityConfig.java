5c6c6c941650e1880d3f3159d375f2a1ede8565c
김멍몽 <wjdahrrla@gmail.com>
2024-01-24 00:52:48 +0900

    202401240052

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.jwt.config;
 
+import com.example.jwt.filter.MyFilter1;
+import com.example.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 
@@ -24,16 +28,17 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class)
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다.
                 )
                 .addFilter(corsFilter) // @CrossOrigin(인증x), 시큐리티 필터에 등록 인증(o)
                 .formLogin(login -> login
                         .disable()
                 )
                 .httpBasic(basic -> basic
-                        .disable()
+                        .disable() // basic 방식은 id와 pw를 보내기 떄문에 노출이 될 가능성이 크다. 그래서 bearer token 방법을 쓴다.
                 )
 //                .apply(new MyCustomDsl()) // 커스텀 필터 등록
                 .authorizeRequests(authorize -> authorize
